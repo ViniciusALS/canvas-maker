893afc0dd3b1d83d1816784249ac724552656a5d
Vinicius Souza <contact@viniciusals.com>
2022-04-20 02:23:53 -0400

    Reset frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateAccountPageComponent } from './create-account-page/create-account-page.component';
-import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'createAccount', component: CreateAccountPageComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { CreateAccountFormComponent } from './create-account-form/create-account-form.component';
-import { CreateAccountPageComponent } from './create-account-page/create-account-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    TopBarComponent,
-    CreateAccountFormComponent,
-    CreateAccountPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
